82e9770f6b3d7d2dbc80f0c36765f4932582cc4e
Shane dela Rosa <delarosa.shane19@gmail.com>
2016-09-06 16:54:07 +0800

    added Bearer in router

--- web/router.ex
+++ web/router.ex
@@ -9,7 +9,7 @@ defmodule Peepchat.Router do
    # Authenticated Requests
   pipeline :api_auth do
     plug :accepts, ["json", "json-api"]
-    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     plug Guardian.Plug.LoadResource
   end
 
@@ -26,5 +26,5 @@ defmodule Peepchat.Router do
     pipe_through :api_auth
     get "/user/current", UserController, :current
   end
-  
+
 end
